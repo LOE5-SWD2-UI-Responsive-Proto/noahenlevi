69314321c75f86a75673628c973235511a4430ec
levigeerlings <6052305@mborijnland.nl>
2026-04-01 11:00:31 +0200

    code

---
+++ Open.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="JOUW NAAM">
+    <title>Openingstijden</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<?php include 'header.php'; ?>
+
+<main>
+
+<div class="overlay"></div> 
+    <section class="times"> 
+     <div class="column"> 
+        <h2>Opening</h2> 
+        <p data-day="1">Maandag: Closed</p> 
+        <p data-day="2">Dinsdag: 11:00 - 22:00</p> 
+        <p data-day="3">Woensdag: 11:00 - 22:00</p> 
+        <p data-day="4">Donderdag: 12:00 - 22:00</p> 
+        <p data-day="5">Vrijdag: 12:00 - 22:00</p> 
+        <p data-day="6">Zaterdag: 12:00 - 22:00</p> 
+        <p data-day="0">Zondag: 12:00 - 22:00</p> 
+    </div> 
+    <div class="column"> 
+        <h2>Delivery</h2> 
+        <p data-day="1">Maandag: Closed</p> 
+        <p data-day="2">Dinsdag: 15:00 - 21:00</p> 
+        <p data-day="3">Woensdag: 15:00 - 21:00</p> 
+        <p data-day="4">Donderdag: 15:00 - 21:00</p> 
+        <p data-day="5">Vrijdag: 15:00 - 21:00</p> 
+        <p data-day="6">Zaterdag: 15:00 - 21:00</p> 
+        <p data-day="0">Zondag: 11:00 - 21:00</p> 
+    </div> 
+    <div class="column"> 
+        <h2>Take Away</h2> 
+        <p data-day="1">Maandag: Closed</p> 
+        <p data-day="2">Dinsdag: 12:00 - 22:00</p> 
+        <p data-day="3">Woensdag: 12:00 - 22:00</p> 
+        <p data-day="4">Donderdag: 12:00 - 22:00</p> 
+        <p data-day="5">Vrijdag: 12:00 - 22:00</p> 
+        <p data-day="6">Zaterdag: 12:00 - 22:00</p> 
+        <p data-day="0">Zondag: 12:00 - 24:00</p> 
+    </div>
+</section>
+
+</main>
+
+<?php include 'footer.php'; ?>
